15090977511dbf4166355f6d6eccf0b85c9c15cd
Nick Yahnke <nick@pitchinteractive.com>
2022-04-07 13:05:11 -0700

    add router

--- prototypes/graphs/src/App.js
+++ prototypes/graphs/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Graph from './Graph'
+import PPAHDIGraph from './PPAHDIGraph'
+import {   BrowserRouter,
+  Routes,
+  Route, NavLink} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Graph />
+      <BrowserRouter>
+        <div>
+          <NavLink to='/'>Indicies Graphs</NavLink>{' '}
+          <NavLink to='/ppaHDI'>Planetary pressures adjusted HDI</NavLink>
+        </div>
+        <Routes>
+          <Route path='/ppaHDI' element={<PPAHDIGraph />} />
+          <Route path='/' element={<Graph />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
