72f988fbd43cb483512bff0d29cca55a23f876a1
Nick Yahnke <nick@pitchinteractive.com>
2022-04-26 13:54:24 -0700

    add remaining country index descriptions

--- prototypes/graphs/src/getCountryIndexDescription.js
+++ prototypes/graphs/src/getCountryIndexDescription.js
@@ -45,6 +45,43 @@ export default function getCountryIndexDescription(country, index, data) {
         </p>
       </>
     )
+  } else if (index.key === 'IHDI') {
+    const lossFormatted = format(country[`loss_${latestYear}`], 'loss')
+    description = (
+      <p>
+        The IHDI considers inequalities in all three dimensions of the HDI by ‘discounting’ each dimension’s average value according to its level of inequality in the distribution. The ‘loss’ in human development due to inequality is given by the difference between the HDI and the IHDI. As the inequality in a country increases, the loss in human development also increases. {getCountryWithApostrophe(country.Country)} loss due to inequality is {lossFormatted} percent, which lowers the HDI to {latestValueFormatted} in {latestYear}.
+      </p>
+    )
+  } else if (index.key === 'GDI') {
+    const femaleHdi = format(country[`hdi_f_${latestYear}`], 'hdi_f')
+    const maleHdi = format(country[`hdi_m_${latestYear}`], 'hdi_m')
+
+    description = (
+      <p>
+        The GDI measures gender gaps in achievements in three basic dimensions of human development: health (measured by female and male life expectancy at birth), knowledge (measured by female and male expected years of schooling for children and mean years of schooling for adults aged 25 years and older) and living standards (measured by female and male estimated GNI per capita). It is a ratio of the female to the male HDI. The {latestYear} female HDI value for {country.Country} is {femaleHdi} in contrast with {maleHdi} for males, resulting in a GDI value of {latestValueFormatted}, placing it into Group {country[`gdi_group_${latestYear}`]}.
+      </p>
+    )
+  } else if (index.key === 'GII') {
+    const countCountriesWithGII = data.filter(country => {
+      return country.ISO3 && country[`gii_${latestYear}`] !== ''
+    }).length
+    description = (
+      <p>
+        The GII measures gender inequalities (the loss in human development due to inequality between female and male achievements) in three key dimensions – reproductive health, empowerment, and labour market. Reproductive health is measured by maternal mortality ratio and adolescent birth rates; empowerment is measured by the shares of parliamentary seats held and population with at least some secondary education by each gender; and labour market participation is measured by the labour force participation rates for women and men. {country.Country} has a GII value of {latestValueFormatted}, ranking it {country[`gii_rank_${latestYear}`]} out of {countCountriesWithGII} countries in {latestYear}.
+      </p>
+    )
+  } else if (index.key === 'MPI') {
+    description = (
+      <p>
+        The MPI looks beyond income to understand how people experience poverty in multiple and simultaneous ways. It identifies how people are being left behind across three key dimensions: health, education and standard of living, comprising 10 indicators. People who experience deprivation in at least one third of these weighted indicators fall into the category of multidimensionally poor.
+      </p>
+    )
+  } else if (index.key === 'PHDI') {
+    description = (
+      <p>
+        The PHDI discounts the HDI for pressures on the planet to reflect a concern for intergenerational inequality. It is the level of human development adjusted by carbon dioxide emissions per person (production-based) and material footprint per capita to account for the excessive human pressure on the planet. In an ideal scenario where there are no pressures on the planet, the PHDI equals the HDI. However, as pressures increase, the PHDI falls below the HDI. In this sense, the PHDI measures the level of human development when planetary pressures are considered.
+      </p>
+    )
   }
 
   return description || defaultDescription
