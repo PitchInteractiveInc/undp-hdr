9092cbf20edb560e94273d3eeffee6406a2d541c
Nick Yahnke <nick@pitchinteractive.com>
2022-04-28 10:52:01 -0700

    refactor index graph for performance

--- prototypes/graphs/src/IndexGraph.js
+++ prototypes/graphs/src/IndexGraph.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useMemo } from 'react'
 import useHDRData from "./useHDRData";
 import { range } from 'd3-array'
 import { scaleLinear, scaleQuantize } from 'd3-scale'
@@ -29,8 +29,20 @@ export const colors = [
   '#3288ce',
   '#006eb5',
 ]
-export default function IndexGraph(props) {
+export default function IndexGraphWrapper(props) {
   const { data } = useHDRData()
+  const { selectedMetricShortName } = useParams()
+  const indicator = indicators.find(d => d.key === selectedMetricShortName)
+  if (indicator.customGraph) {
+    return indicator.customGraph
+  }
+  if (!data) {
+    return null
+  }
+  return <IndexGraph {...props} data={data} />
+}
+function IndexGraph(props) {
+  const { data } = props
   const { selectedMetricShortName } = useParams()
   const [selectedCountries, setSelectedCountries] = useState(Array.from({length: 3}).map(() => ''))
   const [selectedRegion, setSelectedRegion] = useState('')
@@ -39,18 +51,12 @@ export default function IndexGraph(props) {
   const indicator = indicators.find(d => d.key === selectedMetricShortName)
   const index = indicator
   const svgRef = useRef()
-  if (indicator.customGraph) {
-    return indicator.customGraph
-  }
   // console.log(data)
 
-  if (!data) {
-    return null
-  }
 
   const dataKey = indicator.key
   const countSelectedCountries = selectedCountries.filter(d => d !== '').length
-  const graphColumns = getGraphColumnsForKey(data, dataKey)
+  const graphColumns = useMemo(() => getGraphColumnsForKey(data, dataKey), [data, dataKey])
   // console.log(dataKey, data.columns)
   // console.log(graphColumns)
 
@@ -60,127 +66,134 @@ export default function IndexGraph(props) {
   const svgWidth = width + margins.left + margins.right
   const svgHeight = height + margins.top + margins.bottom
 
-  const saveSVG = (event) => {
-    exportSVG(event.target.closest('svg'), `${indicator.name}.svg`)
-  }
 
-
-  const yearExtent = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
-  const yExtent = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
-  data.forEach(country => {
-    graphColumns.forEach(col => {
-      const value = +country[col]
-      if (country[col] !== '') {
-        const year = +col.substr(col.lastIndexOf('_') + 1)
-        yearExtent[0] = Math.min(yearExtent[0], year)
-        yearExtent[1] = Math.max(yearExtent[1], year)
-        yExtent[0] = Math.min(yExtent[0], value)
-        yExtent[1] = Math.max(yExtent[1], value)
-      }
+  const { yearExtent, yExtent } = useMemo(() => {
+    const yearExtent = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
+    const yExtent = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
+    data.forEach(country => {
+      graphColumns.forEach(col => {
+        const value = +country[col]
+        if (country[col] !== '') {
+          const year = +col.substr(col.lastIndexOf('_') + 1)
+          yearExtent[0] = Math.min(yearExtent[0], year)
+          yearExtent[1] = Math.max(yearExtent[1], year)
+          yExtent[0] = Math.min(yExtent[0], value)
+          yExtent[1] = Math.max(yExtent[1], value)
+        }
+      })
     })
-  })
+    return { yearExtent, yExtent }
+  }, [data, graphColumns])
   const countries = data.filter(d => d.ISO3 !== '')
 
   // console.log(graphColumns)
 
-  const xScale = scaleLinear()
+  const xScale = useMemo(() => scaleLinear()
     .domain([0, graphColumns.length - 1])
     .range([0, width])
+  , [graphColumns.length])
 
-  const yScale = scaleLinear()
+  const yScale = useMemo(() => scaleLinear()
     .domain(yExtent)
     .range([height, 0])
+  , [yExtent])
 
-  const colorScale = scaleQuantize()
+  const colorScale = useMemo(() => scaleQuantize()
     .domain(yExtent)
     .range(colors)
-  let selectedDots = []
-  const delaunayData = []
+  , [yExtent])
+  const {paths, delaunay, delaunayData, selectedDots} = useMemo(() => {
 
-  const paths = data.filter(d => d.ISO3 !== '' || d.Country === 'World').map(country => {
-    const isWorld = country.Country === 'World'
-    if (!isWorld && selectedRegion !== '') {
-      if (country.region !== selectedRegion) {
+
+    let selectedDots = []
+
+    const delaunayData = []
+    const paths = data.filter(d => d.ISO3 !== '' || d.Country === 'World').map(country => {
+      const isWorld = country.Country === 'World'
+      if (!isWorld && selectedRegion !== '') {
+        if (country.region !== selectedRegion) {
+          return null
+        }
+      }
+      const data = graphColumns.map((col, colIndex) => {
+        if (country[col] === '') {
+          return null
+        }
+        const value = +country[col]
+        return {
+          value,
+          year: getYearOfColumn(col),
+          col,
+          colIndex,
+        }
+      }).filter(d => d)
+
+      const lineGen = line()
+        .x(d => xScale(d.colIndex))
+        .y(d => yScale(d.value))
+      if (data.length === 0) {
         return null
       }
-    }
-    const data = graphColumns.map((col, colIndex) => {
-      if (country[col] === '') {
-        return null
-      }
-      const value = +country[col]
-      return {
-        value,
-        year: getYearOfColumn(col),
-        col,
-        colIndex,
-      }
-    }).filter(d => d)
+      const stroke = isWorld ? 'black' : colorScale(data[0].value)
+      const strokeWidth = isWorld ? 2 : 1
+      let label = null
+      let opacity = 1
+      let showLabel = null
+      data.forEach(datum => {
+        const x = xScale(datum.colIndex)
+        const y = yScale(datum.value)
+        if (countSelectedCountries === 0 || (selectedCountries.includes(country.ISO3) || isWorld))  {
+          delaunayData.push([x, y, {row: country, col: datum.col}])
+        }
+      })
+      if (countSelectedCountries !== 0 && !isWorld) {
+        const isSelected = selectedCountries.includes(country.ISO3)
+        opacity = isSelected ? 1 : 0.1
+        showLabel = isSelected
 
-    const lineGen = line()
-      .x(d => xScale(d.colIndex))
-      .y(d => yScale(d.value))
-    if (data.length === 0) {
-      return null
-    }
-    const stroke = isWorld ? 'black' : colorScale(data[0].value)
-    const strokeWidth = isWorld ? 2 : 1
-    let label = null
-    let opacity = 1
-    let showLabel = null
-    data.forEach(datum => {
-      const x = xScale(datum.colIndex)
-      const y = yScale(datum.value)
-      if (countSelectedCountries === 0 || (selectedCountries.includes(country.ISO3) || isWorld))  {
-        delaunayData.push([x, y, {row: country, col: datum.col}])
+        if (isSelected) {
+          selectedDots.push(<g key={country.ISO3}>
+            {data.map(datum => {
+              return <circle
+                key={datum.year}
+                cx={xScale(datum.colIndex)}
+                cy={yScale(datum.value)}
+                r={3}
+                fill={stroke}
+                opacity={opacity}
+              />
+            })}
+          </g>)
+        }
       }
+      if (isWorld) {
+        showLabel = true
+        opacity = 1
+      }
+      if (showLabel) {
+        const x = xScale(data[0].colIndex)
+        label = <text fill={stroke} dx='0.2em' fontWeight='bold' fontSize='1.5em' dy='-1em' x={x} y={yScale(data[0].value)}>{country.Country}</text>
+
+      }
+      return (
+        <g key={country.ISO3}>
+          <path
+            opacity={opacity}
+            strokeWidth={strokeWidth}
+            className='graphPath'
+            d={lineGen(data)}
+            fill="none"
+            stroke={stroke}
+            strokeDasharray='1,1'
+            style={{ filter: `drop-shadow(0px 0px 3px ${stroke})` }}
+          />
+          {label}
+        </g>
+      )
     })
-    if (countSelectedCountries !== 0 && !isWorld) {
-      const isSelected = selectedCountries.includes(country.ISO3)
-      opacity = isSelected ? 1 : 0.1
-      showLabel = isSelected
-
-      if (isSelected) {
-        selectedDots.push(<g key={country.ISO3}>
-          {data.map(datum => {
-            return <circle
-              key={datum.year}
-              cx={xScale(datum.colIndex)}
-              cy={yScale(datum.value)}
-              r={3}
-              fill={stroke}
-              opacity={opacity}
-            />
-          })}
-        </g>)
-      }
-    }
-    if (isWorld) {
-      showLabel = true
-      opacity = 1
-    }
-    if (showLabel) {
-      const x = xScale(data[0].colIndex)
-      label = <text fill={stroke} dx='0.2em' fontWeight='bold' fontSize='1.5em' dy='-1em' x={x} y={yScale(data[0].value)}>{country.Country}</text>
-
-    }
-    return (
-      <g key={country.ISO3}>
-        <path
-          opacity={opacity}
-          strokeWidth={strokeWidth}
-          className='graphPath'
-          d={lineGen(data)}
-          fill="none"
-          stroke={stroke}
-          strokeDasharray='1,1'
-          style={{ filter: `drop-shadow(0px 0px 3px ${stroke})` }}
-        />
-        {label}
-      </g>
-    )
-  })
-  const delaunay = Delaunay.from(delaunayData)
+    const delaunay = Delaunay.from(delaunayData)
+    return { paths, delaunay, delaunayData, selectedDots }
+  }, [selectedCountries, selectedRegion, graphColumns, data, xScale, yScale, colorScale, countSelectedCountries])
 
   const columnWidth = xScale(1)
 
