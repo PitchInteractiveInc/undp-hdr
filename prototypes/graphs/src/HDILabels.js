c5aae8a9d59af04a8eb8da4296edbb2fe9ccee71
Nick Yahnke <nick@pitchinteractive.com>
2022-05-19 15:57:02 -0700

    stack hdi labels

--- prototypes/graphs/src/HDILabels.js
+++ prototypes/graphs/src/HDILabels.js
@@ -6,15 +6,17 @@ export default function HDILabels(props) {
   return (
     <div className={classNames('HDILabels', { inline })} style={{ width }}>
       <div style={{ marginRight: '1em', fontSize: '0.875em'}}>HDI classification (value):</div>
-      {hdiBackgroundRectData.map((backgroundRect, i) => {
-        return (
+      <div className='labels'>
+        {hdiBackgroundRectData.map((backgroundRect, i) => {
+          return (
 
-          <div key={i} className='hdiValue' >
-            <div className='background' style={{ opacity: backgroundRect.opacity, backgroundColor: backgroundRect.fill }} />
-            {backgroundRect.label}
-          </div>
-        )
-      })}
+            <div key={i} className='hdiValue' >
+              <div className='background' style={{ opacity: backgroundRect.opacity, backgroundColor: backgroundRect.fill }} />
+              {backgroundRect.label}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 
